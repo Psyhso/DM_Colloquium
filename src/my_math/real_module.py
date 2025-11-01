0d8389de407ee5fbacf3e961fa6ea59ae91d6168
aishakurov_24 <Albert8666@yandex.ru>
2025-11-01 16:41:47 +0300

    feature/reall_DEG_P_N

--- src/my_math/real_module.py
+++ src/my_math/real_module.py
@@ -1,2 +1,7 @@
 class RealModule:
-    pass
+    def DEG_P_N(self):
+        """
+        Шакуров 4384 
+        Возвращает степень многочлена (количество коэффициентов - 1)
+        """
+        return len(self.C) - 1
