2650deb1a8f77c3b97f4ccc0599ee887bf9a2ae9
Vitaliy Vodolazko <vodolazko2000@mail.ru>
2025-11-01 13:33:56 +0300

    real_MUL_Pxk_P

--- src/my_math/real_module.py
+++ src/my_math/real_module.py
@@ -1,2 +1,17 @@
 class RealModule:
-    pass
+    def __init__(self, m : int, C : list):
+        self.m = m
+        self.C = C
+    
+    def MUL_Pxk_P(self, k : int):
+        """
+        Водолазко 4384
+        Принимает на вход натуральное число k, степень на которую необходимо умножить полином
+        """
+        if k < 0:
+            print("k должно быть положительным")
+            return self
+        new_C = self.C + [0] * k
+        self.C = new_C
+        self.m += k
+        return self
