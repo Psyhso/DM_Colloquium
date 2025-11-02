4b3c4d431ca135b38c9e1a53debc43d9b52ef76e
Psyhso <131555003+Psyhso@users.noreply.github.com>
2025-11-02 22:14:34 +0300

    Update real_module.py

--- src/my_math/real_module.py
+++ src/my_math/real_module.py
@@ -31,3 +31,24 @@ class RealModule:
         """
         return len(self.C) - 1
 
+    def DER_P_P(self):
+        """
+        Шакуров 4384 
+        Возвращает степень многочлена (количество коэффициентов - 1)
+        Шакуров 4384
+        Возвращает производную многочлена
+        Если многочлен константа, его производная = 0
+        Иначе каждый коэффициент (кроме свободного члена) умножаем на степень
+        """
+        return len(self.C) - 1
+        if len(self.C) == 1:
+            self.C = [0]
+            self.m = 0
+            return self
+        for i in range(len(self.C) - 1):
+            power = len(self.C) - 1 - i
+            self.C[i] = self.C[i] * power
+        self.C = self.C[:-1]
+        self.m -= 1
+        return self
+
