1de6303127b070327998e2e9d05c89cf89cb7883
OvcharenkoYa <ricric6703@gmail.com>
2025-11-09 02:45:24 +0300

    feature/rational_module

--- src/my_math/rational_module.py
+++ src/my_math/rational_module.py
@@ -14,12 +14,11 @@ class RationalModule:
 
         Алгоритм: Находим НОД модуля числителя и знаменателя, затем сокращаем дробь, деля числитель и знаменатель на НОД
         """
-        abs_up = self.up.ABS_Z_Z().TRANS_Z_N()
-
-        gcd = abs_up.GCF_NN_N(self.down)
-        
-        self.up = self.up.DIV_ZZ_Z(IntegerModule(0, gcd.n, gcd.A))
-        self.down = self.down.DIV_NN_N(gcd)
+        abs_up = NaturalModule(self.up.n, self.up.A)
+        gcd = abs_up.GCF_NN_N(NaturalModule(self.down.n, self.down.A))
+        if not (gcd.A[0] == 1 and gcd.n == 0):
+            self.up = self.up.DIV_ZZ_Z(IntegerModule(0, gcd.n, gcd.A))
+            self.down.DIV_NN_N(gcd)
         return self
 
     def INT_Q_B(self) -> str:
@@ -66,14 +65,14 @@ class RationalModule:
         Алгоритм: Находим НОК знаменателей, вычисляем дополнительные множители, затем приводим дроби к общему знаменателю и складываем
         """
         lcm = NaturalModule(self.down.n, self.down.A)
-        lcm.LCM_NN_N(other.down)
-        
-        m1 = lcm.DIV_NN_N(self.down)
-        m2 = lcm.DIV_NN_N(other.down)
+        lcm.LCM_NN_N(NaturalModule(other.down.n, other.down.A))
+
+        m1 = NaturalModule(lcm.n, lcm.A).DIV_NN_N(self.down)
+        m2 = NaturalModule(lcm.n, lcm.A).DIV_NN_N(other.down)
         
         new_up1 = self.up.MUL_ZZ_Z(IntegerModule(0, m1.n, m1.A))
         new_up2 = other.up.MUL_ZZ_Z(IntegerModule(0, m2.n, m2.A))
-        
+
         self.up = new_up1.ADD_ZZ_Z(new_up2)
         self.down = lcm
         return self.RED_Q_Q()
@@ -86,14 +85,14 @@ class RationalModule:
         Алгоритм: Находим НОК знаменателей, вычисляем дополнительные множители, затем приводим дроби к общему знаменателю и вычитаем
         """
         lcm = NaturalModule(self.down.n, self.down.A)
-        lcm.LCM_NN_N(other.down)
+        lcm.LCM_NN_N(NaturalModule(other.down.n, other.down.A))
         
-        m1 = lcm.DIV_NN_N(self.down)
-        m2 = lcm.DIV_NN_N(other.down)
+        m1 = NaturalModule(lcm.n, lcm.A).DIV_NN_N(self.down)
+        m2 = NaturalModule(lcm.n, lcm.A).DIV_NN_N(other.down)
         
         new_up1 = self.up.MUL_ZZ_Z(IntegerModule(0, m1.n, m1.A))
         new_up2 = other.up.MUL_ZZ_Z(IntegerModule(0, m2.n, m2.A))
-        
+
         self.up = new_up1.SUB_ZZ_Z(new_up2)
         self.down = lcm
         return self.RED_Q_Q()
@@ -106,7 +105,7 @@ class RationalModule:
         Алгоритм: Умножаем числители и знаменатели части дробей, затем сокращаем дробь
         """
         self.up = self.up.MUL_ZZ_Z(other.up)
-        self.down = self.down.MUL_NN_N(other.down)
+        self.down.MUL_NN_N(other.down)
         return self.RED_Q_Q()
 
     def DIV_QQ_Q(self, other):
@@ -117,9 +116,9 @@ class RationalModule:
         Алгоритм: Умножаем на обратную дробь, учитываем знак и сокращаем итоговую
         """
         self.up = self.up.MUL_ZZ_Z(IntegerModule(other.up.b, other.down.n, other.down.A))
-        self.down = self.down.MUL_NN_N(NaturalModule(other.up.n, other.up.A))
+        self.down.MUL_NN_N(NaturalModule(other.up.n, other.up.A))
         return self.RED_Q_Q()
 
     def __str__(self):
         sign = "-" if self.up.b else ""
-        return f"{sign}{''.join(map(str, self.up.A))}/{''.join(map(str, self.down.A))}"
+        return f"{sign}{''.join(map(str, self.up.A[::-1]))}/{''.join(map(str, self.down.A[::-1]))}"
