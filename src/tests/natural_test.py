3121bc532c60749799b0bddc6d7b357e89c4f5cd
Psyhso <stukalkin06@bk.ru>
2025-11-16 23:19:51 +0300

    Поправил тесты

--- src/tests/natural_test.py
+++ src/tests/natural_test.py
@@ -1,5 +1,5 @@
 import pytest
-from my_math.natural_module import NaturalModule
+from ..my_math.natural_module import NaturalModule
 
 
 # Тесты для COM_NN_D (N-1)
--- src/tests/real_test.py
+++ src/tests/real_test.py
@@ -1,8 +1,8 @@
 import pytest
-from my_math.real_module import RealModule
-from my_math.natural_module import NaturalModule
-from my_math.integer_module import IntegerModule
-from my_math.rational_module import RationalModule
+from ..my_math.real_module import RealModule
+from ..my_math.natural_module import NaturalModule
+from ..my_math.integer_module import IntegerModule
+from ..my_math.rational_module import RationalModule
 
 def create_natural_from_int(num: int):
     """Создает NaturalModule из целого числа"""
